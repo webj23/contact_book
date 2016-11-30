d659e4924e89ab5b0fd2d6668f9e10becd9d247d
webj23 <webj.kr@gmail.com>
2016-11-30 18:05:55 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var mongoose = require('mongoose');
+var app = express();
+
+mongoose.connect(precess.env.MONGO_DB);
+var db = mongoose.connection;
+
+db.once('open', function() {
+  console.log('DB connection');
+});
+
+db.on('error',function(err) {
+  console.log('DB ERROR : ', err);
+});
+
+app.set('view engine','ejs');
+app.use(express.static(__dirname + '/public'));
+
+app.listen(3000, function() {
+  console.log('server on!!');
+});
