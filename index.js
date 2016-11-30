449010b9e7b893a7b96a3d083c31f2200cd9ad26
webj23 <webj.kr@gmail.com>
2016-12-01 01:18:36 +0900

    work at home

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var mongoose = require('mongoose');
 var app = express();
 
-mongoose.connect(precess.env.MONGO_DB);
+mongoose.connect(process.env.MONGO_DB);
 var db = mongoose.connection;
 
 db.once('open', function() {
